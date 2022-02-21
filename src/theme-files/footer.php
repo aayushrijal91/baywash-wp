0ce8f3d6b8b031d472b31b3c70e21fd51468c6b0
Aayush Rijal <aayush.rijal99@gmail.com>
2022-02-21 12:04:03 +1100

    WIP footer

--- src/theme-files/footer.php
+++ src/theme-files/footer.php
@@ -1,7 +1,55 @@
-<footer></footer>
+<?php
+$footer_logo = get_field('footer_logo', 'options');
+?>
+<footer class="footer bg-black">
+	<div class="container">
+		<div class="row justify-content-between">
+			<div class="col-xl-4 col-xxl-3">
+				<div class="">
+					<a href="<?= home_url() ?>"><img src="<?= $footer_logo['url']; ?>" alt="<?= $footer_logo['alt']; ?>" /></a>
+				</div>
+				<div class="footer-description">
+					<div class="pr-4">
+						<?= get_field('description', 'options') ?>
+					</div>
+				</div>
+				<div class="social-media">
+					<?php if (have_rows('social_media', 'option')) : ?>
+						<?php while (have_rows('social_media', 'option')) : the_row();
+							$social_icon = get_sub_field('social_icon');
+							$social_url = get_sub_field('social_media_url');
+						?>
+							<a href="<?= $social_url ?>" class="social-icon" target="_blank"><img src="<?= $social_icon['url'] ?>" alt="<?= $social_icon['alt'] ?>"></a>
+						<?php endwhile; ?>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-between footer-bottom">
+			<div class="col-auto copyright">
+				<?= get_field('copyright_text', 'option') ?>
+			</div>
+			<div class="col-auto">
+				<div class="row no-gutters justify-content-center justify-content-lg-end align-items-center">
+					<div class="col-auto">
+						<a href="<?= get_field('privacy_policy_url', 'option')['url'] ?>" class="text-white fw-300 h24">Privacy Policy</a>
+					</div>
+					<div class="col-auto px-4">|</div>
+					<div class="col-auto">
+						<a href="<?= get_field('terms_and_conditions_url', 'option')['url'] ?>" class="text-white fw-300 h24">Terms and Conditions</a>
+					</div>
+					<div class="col-auto pl-lg-5 pl-xxl-7">
+						<a href="<?= get_field('company_link', 'option')['url'] ?>" target="_blank"><img src="<?= get_field('company_logo', 'option')['url'] ?>" alt="<?= get_field('company_logo', 'option')['alt'] ?>"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
 <a href="javascript:" id="return-to-top">
-		<img src="images/icons/arrow-up.webp">
-	</a>
+	<img src="images/icons/arrow-up.webp">
+</a>
 <?php wp_footer(); ?>
 </body>
+
 </html>
--- src/theme-files/header.php
+++ src/theme-files/header.php
@@ -4,6 +4,9 @@
  * The template for displaying the header
  * 
  */
+
+$logo = get_field('logo', 'options');
+$phone_number = get_field('phone_number', 'options');
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -16,10 +19,36 @@
 </head>
 
 <body <?php body_class(); ?>>
-    <header>
+    <header class="header">
         <div class="header-top-bar bg-gray">
             <div class="container">
-                <div class="text-right text-primary fw-400 py-3">Contact us for a <span class="fw-800">free quote</span>, one of our friendly team members is ready to assist</div>
+                <div class="text-right text-primary fw-400 py-3"><?= get_field('hero_slogan', 'options') ?></div>
+            </div>
+        </div>
+        <div class="header-navigation-bar">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light px-0">
+                    <a class="navbar-brand" href="<?= home_url() ?>"><img src="<?= $logo['url']; ?>" alt="<?= $logo['alt']; ?>" /></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarText">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                        </ul>
+                        <a class="btn btn-secondary rounded-pill text-tertiary h21 fw-800 px-4 line-height-1 py-3" href="tel:<?= $phone_number ?>">
+                            <?= $phone_number ?>
+                        </a>
+                    </div>
+                </nav>
             </div>
         </div>
     </header>
