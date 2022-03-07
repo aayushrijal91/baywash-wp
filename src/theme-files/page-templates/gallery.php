5dd12d99548ba9514a679f1e272eef3c5c9bd08e
Aayush Rijal <aayush.rijal99@gmail.com>
2022-03-07 16:15:42 +1100

    Gallery page done

--- src/theme-files/page-templates/gallery.php
+++ src/theme-files/page-templates/gallery.php
@@ -8,6 +8,25 @@ get_header();
 get_template_part('parts/section', 'banner');
 ?>
 
+<section class="gallery">
+	<div class="container">
+		<div class="grid-wrapper">
+			<?php if (have_rows('gallery_images')) : ?>
+				<?php while (have_rows('gallery_images')) : the_row();
+					$image = get_sub_field('image');
+					$wide = get_sub_field('wide');
+				?>
+					<div class="<?= $wide ? "wide" : '' ?>">
+						<img src="<?= $image['url'] ?>" alt="<?= $image['alt'] ?>">
+						<div class="overlay"></div>
+					</div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+</section>
+
 <?php
-	get_footer();
+get_template_part('parts/section', 'testimonials');
+get_footer();
 ?>
